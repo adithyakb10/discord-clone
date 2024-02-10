f6272c9f9b5d2612c21217adcfe378e3d34aa42f
Adithya K B <adithyakb93@gmail.com>
2024-02-10 14:27:27 +0530

    initialized database using prisma

---
+++ app/(setup)/page.tsx
@@ -0,0 +1,23 @@
+import { db } from "@/lib/db";
+import { initialProfile } from "@/lib/intial-profile";
+import { redirect } from "next/navigation";
+
+export default async function SetupPage() {
+  const profile = await initialProfile();
+
+  const server = await db.server.findFirst({
+    where: {
+      Member: {
+        some: {
+          profileId: profile.id,
+        },
+      },
+    },
+  });
+
+  if (server) {
+    return redirect(`/server/${server.id}`);
+  }
+
+  return <div>Create a server</div>;
+}
